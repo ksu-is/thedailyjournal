72b0aca713714f03b00df22c0d734ebb65f62843
ayeger <51214831+ayeger@users.noreply.github.com>
2019-07-13 21:33:46 -0400

    Collection
    a folder to converge all classes.

---
+++ Final/textField.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+class textField:
+
+    root = tk.Tk()
+    S = tk.Scrollbar(root)
+    vget = root.winfo_screenheight()
+    print("Screen Height:",vget, "(Used for textbox Height.)")
+    T = tk.Text(root, height=vget, width=80)
+    S.pack(side=tk.RIGHT, fill=tk.Y)
+    T.pack()
+    S.config(command=T.yview)
+    T.config(yscrollcommand=S.set)
+    T.insert(tk.END, "")
+
+textField()
+tk.mainloop()
