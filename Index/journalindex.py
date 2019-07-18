f613411321e7473e4c9930f14fc1cf6eeb0a3b30
ayeger <51214831+ayeger@users.noreply.github.com>
2019-07-18 18:16:50 -0400

    Update journalindex.py

--- Index/journalindex.py
+++ Index/journalindex.py
@@ -8,7 +8,7 @@ class Journal:
 
     root = Tk()
 
-    windowidth = 800
+    windowwidth = 800
     windowheight = 600
     textarea = Text(root)
     menubar = Menu(root)
@@ -18,6 +18,27 @@ class Journal:
 
     scrollbar = Scrollbar(textarea)
     file = None
+
+    def __init__(self,**kwargs):
+
+        self.windowwidth = kwargs['width']
+        self.windowheight = kwargs['height']
+
+        self.root.title("Untitled - Journal Entry")
+
+        screenWidth = self.root.winfo_screenwidth()
+        screenHeight = self.root.winfo_screenheight()
+
+        alignleft = (screenWidth / 2) - (self.windowwidth /2)
+
+        aligntop = (screenHeight /2) - (self.windowheight /2)
+
+        self.root.geometry('%dx%dx+%dx%dx' % (self.windowwidth, self.windowheight, alignleft, aligntop))
+        
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+
+        self.textarea.grid(sticky = N + E + S + W)
 
 journal = Journal(width=800,height=600)
 journal.run()
